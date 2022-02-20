0dfc1a971e63b2c99419e58498014c90d39cf416
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2022-02-20 14:56:38 +0900

    해시 - 위장

---
+++ 프로그래머스/고득점/위장.py
@@ -0,0 +1,15 @@
+def solution(clothes):
+    closet = {}
+    result = 1
+    for element in clothes:
+        key = element[1]
+        value = element[0]
+        if key in closet:
+            closet[key].append(value)
+        else:
+            closet[key] = [value]
+
+    # -1은 x+1 (안입은 경우 포함) 을 하면서 곱해나가기 때문에 결과에 하나도 안입은 경우가 포함되어서 그 경우를 빼주기 위함입니다.
+    for key in closet.keys():
+        result = result * (len(closet[key]) + 1)
+    return result - 1
