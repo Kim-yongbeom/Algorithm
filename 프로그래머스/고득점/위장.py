3444414dd6d5c8c711df1c9669d218f6ed10e4ba
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2022-03-02 22:54:01 +0900

    해시 - 위장

--- 프로그래머스/고득점/위장.py
+++ 프로그래머스/고득점/위장.py
@@ -13,3 +13,20 @@ def solution(clothes):
     for key in closet.keys():
         result = result * (len(closet[key]) + 1)
     return result - 1
+
+
+# 다른 사람 코드
+def solution(clothes):
+    answer = 1
+    dic = dict()
+
+    for name, kind in clothes:
+        if kind not in dic:
+            dic[kind] = 1
+        else:
+            dic[kind] += 1
+
+    for val in dic.values():
+        answer *= (val + 1)
+
+    return answer - 1
