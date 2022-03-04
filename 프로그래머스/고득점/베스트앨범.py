c38786f2e45b1b2c50625b19eacf01b6d24df993
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2022-03-04 22:49:32 +0900

    해시 - 베스트앨범

--- 프로그래머스/고득점/베스트앨범.py
+++ 프로그래머스/고득점/베스트앨범.py
@@ -8,6 +8,14 @@ def solution(genres, plays):
             answer[key].append(value)
         else:
             answer[key] = [value]
+    # for i in answer.values():
+    #     print(i)
+    # dict값들 뽑아올수있음, 근데 만약
+    # for i in range(answer.values()):
+    #     print(answer.values()[i])
+    # 해주는 거면 안됨
+    # 
+    # dict 에서 .items()를 해주면 튜플 형태로 받아옴
     # 고유번호 4,1 번 더하고 3,0,2 더하게 만들어보자
     keyList = list(answer.keys())
     valueList = list(answer.values())
