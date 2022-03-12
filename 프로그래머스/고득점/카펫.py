d41874374bda21b64985ed9858167764ef90453e
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2022-03-12 13:52:53 +0900

    완전탐색 - 카펫

--- 프로그래머스/고득점/카펫.py
+++ 프로그래머스/고득점/카펫.py
@@ -1,12 +1,23 @@
 # 진행중
 def solution(brown, yellow):
     answer = []
-    count = 1
+    width = 0
+    length = 1
     while yellow > 2:
-        yellow = yellow // count
         if yellow % 2 == 0:
-            count += 1
-    print(count)
-    length = count + 2
+            length += 1
+            yellow = yellow // length
+            # 24 -> 12 ->  4
+            # 1  ->  2 ->  3
+            # 오류 이유 : legnth를 계속 늘어날때 마다 나눠줘서 yellow값이 계속 작아짐
+            # length 값을 계속 올린후 한번에 나눠줘야함
+            if yellow % length != 0:
+                length -= 1
+                yellow = yellow * length
+                break
+    width = yellow
+    print(width)
 
+    answer.append(width + 2)
+    answer.append(length + 2)
     return answer
