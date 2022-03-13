be5cc27668c1e9573ebae92421125365fa2120c2
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2022-03-13 17:04:27 +0900

    완전탐색 - 카펫

--- 프로그래머스/고득점/카펫.py
+++ 프로그래머스/고득점/카펫.py
@@ -1,23 +1,47 @@
-# 진행중
+# 내 코드 38.5점
 def solution(brown, yellow):
     answer = []
     width = 0
     length = 1
-    while yellow > 2:
-        if yellow % 2 == 0:
+    count = yellow  # 타일 갯수 계산
+    while count > 2:
+        if count % 2 == 0:
             length += 1
-            yellow = yellow // length
-            # 24 -> 12 ->  4
-            # 1  ->  2 ->  3
-            # 오류 이유 : legnth를 계속 늘어날때 마다 나눠줘서 yellow값이 계속 작아짐
-            # length 값을 계속 올린후 한번에 나눠줘야함
-            if yellow % length != 0:
+            count = yellow / length
+            #  2 ->  3 -> 4 -> 5
+            # 12 ->  8 -> 6 -> 4(4.8)
+            if int(count*10) % 10 != 0 or length > count:
+                # 소수이거나, 세로 길이가 더 길때 동작
                 length -= 1
-                yellow = yellow * length
                 break
-    width = yellow
-    print(width)
-
+        else:
+            break
+    while count > 3:
+        if count % 3 == 0:
+            length += 2
+            count = yellow / length
+            if int(count*10) % 10 != 0 or length > count:
+                # 소수이거나, 세로 길이가 더 길때 동작
+                length -= 2
+                break
+        else:
+            break
+    width = yellow // length
     answer.append(width + 2)
     answer.append(length + 2)
+    return answer
+
+
+# 다른 사람 코드
+def solution(brown, yellow):
+    answer = []
+    total = brown + yellow                  # a * b = total
+    for b in range(1, (total//2)+1):
+        # 소수 판별
+        if (total / b) % 1 == 0:            # total / b = a
+            a = total / b
+            if a >= b:                      # a >= b
+                if 2*a + 2*b == brown + 4:  # 2*a + 2*b = brown + 4
+                    return [a, b]
+
     return answer
