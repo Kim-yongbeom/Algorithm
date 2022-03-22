2e14d86520142aa7e78dbe771cf0e0fd67805ca1
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2022-03-22 22:37:55 +0900

    깊이/너비 우선 탐색(DFS/BFS) - 단어변환

--- 프로그래머스/고득점/단어변환.py
+++ 프로그래머스/고득점/단어변환.py
@@ -17,10 +17,16 @@ def solution(begin, target, words):
                 break
 
         if score == 1:
+            # 알파벳이 하나 차이나는것 중에서 target과 word 같으면 answer + 1
+            # 밑에서의 target_word와 차이점은
+            # 첫번째의 word가 target이랑 같다면 실행이 안되기 때문에
+            # 이 조건문을 걸어줌
             if target == word:
                 return answer + 1
             answer += 1
             begin = word
+
+            # target 단어에서 word의 알파벳 전부 삭제
             target_word = target.strip(word)
             if len(target_word) == 1:
                 print(word)
