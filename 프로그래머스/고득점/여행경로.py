0e9c22c01ace16cadd49ad4b7c0fbb6485a1f707
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2022-03-24 11:17:06 +0900

    Update 여행경로.py

--- 프로그래머스/고득점/여행경로.py
+++ 프로그래머스/고득점/여행경로.py
@@ -1,27 +1,27 @@
-# 진행중
-from collections import deque
-
-
+# 50점
+# 문제 2개 런타임 에러
 def solution(tickets):
-    answer = []
-    same = []
-    q = deque()
-    for i in range(len(tickets)):
-        q.append([tickets[i][0], tickets[i][1]])
+    answer = ["ICN"]
     V = [0 for i in range(len(tickets))]
-
-    while True:
-        start, target = q.popleft()
-        for i in range(len(tickets)):
-            if start == tickets[i][0]:
-                same.append(tickets[i][1])
-                same = sorted(same)
-        # target를 어떻게 바꿔줄지 생각해보기
-        # tickets[i]를 통해 target값을 반복 조회
-        # start가 ICN 말고 다른 출발지도 같을 경우도 생각해야함
-        if target == same[0]:
-            print(target)
-
-        break
-
+    
+    # 항상 ICN에서 출발
+    for i in range(len(tickets)):
+        if tickets[i][0] == "ICN":
+            start, target = tickets[i]
+    
+    for _ in range(len(tickets)):
+        same = []
+        for i, ticket in enumerate(tickets):
+            if not V[i]:
+                if start == ticket[0]:
+                    same.append([ticket[1], i])
+                    same = sorted(same)
+                    target = same[0][0]
+        if target == same[0][0]:
+            V[same[0][1]] = 1
+            answer.append(target)
+            start = target
+        
     return answer
+
+# 다른 사람 코드
