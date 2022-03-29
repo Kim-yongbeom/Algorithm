6ae1f5c43876203baea0113d6f30e8c2a538c8ca
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2022-03-29 15:53:38 +0900

    탐욕법 - 조이스틱

--- 프로그래머스/고득점/조이스틱.py
+++ 프로그래머스/고득점/조이스틱.py
@@ -44,11 +44,11 @@ def solution(name):
         next = i + 1
         while next < len(name) and name[next] == 'A':
             next += 1
-
+        # ex) JAAJAAAAJ 인 경우 4번째 J를 처리하기 위해서 2, 3번째의 연속된 AA를 건너는게 5,6,7,8번쨰의 연속된 AAAA를 건너는 것 보다 낫다.
         # 기존, 연속된 A의 왼쪽시작 방식, 연속된 A의 오른쪽시작 방식 비교 및 갱신
         min_move = min([min_move, 2 * i + len(name) -
                        next, i + 2 * (len(name) - next)])
-
+    
     # 알파벳 변경(상하이동) 횟수에 좌우이동 횟수 추가
     answer += min_move
     return answer
