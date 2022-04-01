044db201525f89639ebad10b42fc4b2309a45394
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2022-04-01 21:15:42 +0900

    그래프 - 가장 먼 노드

--- 프로그래머스/고득점/가장 먼 노드.py
+++ 프로그래머스/고득점/가장 먼 노드.py
@@ -17,3 +17,25 @@ def solution(n, edge):
                 dic[next_node[1]] += 1
     print(dic)
     return answer
+
+# 다른 사람 코드
+from collections import deque
+def solution(n, edge):
+    answer = 0
+    graph = {}
+    visited = [0]*n
+    for e in edge:
+        graph[e[0]] = graph.get(e[0], []) + [e[1]]
+        graph[e[1]] = graph.get(e[1], []) + [e[0]]
+    queue = deque()
+    queue.append(1)
+    visited[0] = 1
+    while queue:
+        nodes = len(queue)
+        for i in range(nodes):
+            current = queue.popleft()
+            for c in graph[current]:
+                if visited[c-1] == 0:
+                    visited[c-1] = 1
+                    queue.append(c)
+    return nodes
