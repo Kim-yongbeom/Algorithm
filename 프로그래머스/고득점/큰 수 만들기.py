66d12a4ec3a0e9fa3ba06c5cc050fc7ac126da4d
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2022-04-24 17:41:25 +0900

    탐욕법 - 큰 수 만들기

--- 프로그래머스/고득점/큰 수 만들기.py
+++ 프로그래머스/고득점/큰 수 만들기.py
@@ -1,13 +1,26 @@
 # 내 코드
-# 시간초과
-import itertools
-
-
 def solution(number, k):
     answer = ''
-    a = itertools.combinations(number, len(number)-k)
-    answer = max(list(map(''.join, a)))
-    return answer
+    num_list = [0]
+    k += 1
+    for num in number:
+        num = int(num)
+        # num_list 조건을 앞에 써줘야함
+        while num_list and num_list[-1] < num and k > 0:
+            k -= 1
+            num_list.pop()
+        num_list.append(num)
+    # 같은 숫자 반복이여서 k를 다 쓰지 않았는데 반복문이 끝낫을때
+    if k:
+        for i in range(k):
+            num_list.pop()
+    
+    for num in num_list:
+        num = str(num)
+        answer += num
+
+    # 앞자리가 0일때를 생각
+    return str(int(answer))
 
 # 다른 사람 코드
 
