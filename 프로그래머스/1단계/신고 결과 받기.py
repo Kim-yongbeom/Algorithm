06a12c9bc5dfc8529ef2272831d17b44d70d472f
김용범 <subum12345@gmail.com>
2022-05-02 00:28:43 +0900

    1단계 - 신고 결과 받기

--- 프로그래머스/1단계/신고 결과 받기.py
+++ 프로그래머스/1단계/신고 결과 받기.py
@@ -1,3 +1,42 @@
+# 내 코드
+def solution(id_list, report, k):
+    answer = []
+    report = list(set(report))
+    dic, dicR, dicAns = dict(), dict(), dict()
+
+    for i in report:
+        dic[i.split(' ')[0]] = dic.get(i.split(' ')[0], '') + i.split(' ')[1] + ' '
+        dicR[i.split(' ')[1]] = dicR.get(i.split(' ')[1], 0) + 1
+    
+    for id in dicR:
+        if k <= dicR[id]:
+            for i in dic:
+                if id in dic[i]:
+                    dicAns[i] = dicAns.get(i, 0) + 1
+                    
+    for i in id_list:
+        if i not in dicAns:
+            answer.append(0)
+        else:
+            answer.append(dicAns[i])
+    
+    return answer
+
+# 다른 사람 코드
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)    
+    reports = {x : 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+
+    return answer
+
+# 다른 사람 코드
 from collections import defaultdict
 
 
