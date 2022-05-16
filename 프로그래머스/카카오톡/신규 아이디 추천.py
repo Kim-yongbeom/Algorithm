845cff93cf3147636281c3b8677a2790aa63e4fa
김용범 <subum12345@gmail.com>
2022-05-16 14:09:32 +0900

    1단계 - 신규 아이디 추천

---
+++ 프로그래머스/카카오톡/신규 아이디 추천.py
@@ -0,0 +1,37 @@
+def solution(new_id):
+    answer = ''
+    
+    # 1단계
+    new_id = new_id.lower()
+    
+    # 2단계
+    for i in new_id:
+        # isalnum -> 영어, 한글, 숫자면 true 아니면 false
+        if i.isalnum() or i in '-_.':
+            answer += i
+    
+    # 3단계
+    while '..' in answer:
+        answer = answer.replace('..','.')
+    
+    # 4단계
+    if answer and answer[0] == '.':
+        answer = answer[1:]
+    if answer and answer[-1] == '.':
+        answer = answer[:-1]
+    
+    # 5단계
+    if len(answer) == 0:
+        answer += 'a'
+    
+    # 6단계
+    if len(answer) >= 16:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+    
+    # 7단계
+    while len(answer) < 3:
+        answer += answer[-1]
+        
+    return answer
