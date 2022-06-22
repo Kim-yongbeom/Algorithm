4b334cce630c16bc15ac1c511fb0dbfbecd53b92
김용범 <subum12345@gmail.com>
2022-06-22 20:49:35 +0900

    실버3 - N과 M(2)

--- 백준/백트래킹/N과 M(2).py
+++ 백준/백트래킹/N과 M(2).py
@@ -1,3 +1,36 @@
+# 내코드
+n,m = list(map(int,input().split()))
+ 
+s = []
+boolean = False
+ 
+def dfs():
+    if len(s)==m:
+        if len(s) == 1:
+            print(' '.join(map(str,s)))
+            return
+                    
+        else:
+            for a in range(len(s)-1):
+                if s[a] < s[a+1]:
+                    boolean = True
+                else:
+                    boolean = False
+                    break
+                    
+        if boolean:
+            print(' '.join(map(str,s)))
+        return
+    
+    for i in range(1,n+1):
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
+dfs()
+
+
+# 다른사람코드
 n,m = list(map(int,input().split()))
 s = []
 def dfs(start):
